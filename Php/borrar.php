e5114fc2595f0bf316805fddd635cf5fa6caf9f4
BamFavela17 <favela17mx@gmail.com>
2024-05-21 11:54:04 -0700

    Crud Empleado + funcionLogin

---
+++ Php/borrar.php
@@ -0,0 +1,16 @@
+<?php
+function BorrarEmpleado(){
+  try{
+  if (!isset($_GET["id"])) {
+    exit("No hay id");
+}
+$mysqli = include_once "Conexion.php";
+$id = $_GET["id"];
+$sentencia = $mysqli->prepare("DELETE FROM usuario WHERE id = ?");
+$sentencia->bind_param("i", $id);
+$sentencia->execute(); 
+}  
+}
+
+
+?>
